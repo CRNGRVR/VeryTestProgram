798963b30d2cf1a27e56a6354a03c34a6551c789
Ivan <i.a.konuhov@gmail.com>
2023-05-20 20:35:59 +0700

    Initial Commit

---
+++ VeryTestProgram/VeryTestProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeryTestProgramApp.swift
+//  VeryTestProgram
+//
+//  Created by Иван on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct VeryTestProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
